f752220c075cc77ef3ed1fe0738afcf3245c4d06
LucyLu23 <114943897+LucyLu23@users.noreply.github.com>
2023-01-28 11:57:28 -0700

    firstCommitMyPortfolio

---
+++ myportfolio.js
@@ -0,0 +1,317 @@
+const button100 = document.getElementById('button100');
+button100.addEventListener('click', func100);
+function func100(){
+    button100.innerHTML = 'Myportfolio & buildATributePage '
+    button100.style.color = 'blue';
+    button100.style.fontSize = '1.2em;';
+};
+const button101 = document.getElementById('button101');
+button101.addEventListener('click', func101);
+function func101(){
+    button101.innerHTML = 'Website'
+    button101.style.color = 'red';
+    button101.style.fontSize = '1.2em;';
+};
+const button102 = document.getElementById('button102');
+button102.addEventListener('click', func102);
+function func102(){
+    button102.innerHTML = 'DOM,HTML,CSS,JavaScript,CommandJS'
+    button102.style.color = 'green';
+    button102.style.fontSize = '1.2em;';
+    div1.innerHTML = "FocusOn: Basic Knowledge of WebSite Development to organize my Projects and Resume"
+    div1.style.color= "green" 
+};
+
+const button103 = document.getElementById('button103');
+button103.addEventListener('click', func103);
+function func103(){
+    button103.innerHTML = 'SpaceBattleGame & GunGame'
+    button103.style.color = 'blue';
+    button103.style.fontSize = '1.2em;';
+};
+const button104 = document.getElementById('button104');
+button104.addEventListener('click', func104);
+function func104(){
+    button104.innerHTML = 'Game'
+    button104.style.color = 'red';
+    button104.style.fontSize = '1.2em;';
+};
+const button105 = document.getElementById('button105');
+button105.addEventListener('click', func105);
+function func105(){
+    button105.innerHTML = 'Learn more DOM, CSS, Creating Two Pages for the Game '
+    button105.style.color = 'green';
+    button105.style.fontSize = '1.2em;';
+    div1.innerHTML = "FocusOn: React, loop, conditionals, call back function, differetiating class and function(stateful and stateless)"
+    div1.style.color= "green" 
+};
+
+const button106 = document.getElementById('button106');
+button106.addEventListener('click', func106);
+function func106(){
+    button106.innerHTML = ' Pokemon_app, memories_app, & AMERNStackApp'
+    button106.style.color = 'blue';
+    button106.style.fontSize = '1.2em;';
+};
+const button107 = document.getElementById('button107');
+button107.addEventListener('click', func107);
+function func107(){
+    button107.innerHTML = 'App'
+    button107.style.color = 'red';
+    button107.style.fontSize = '1.2em;';
+};
+const button108 = document.getElementById('button108');
+button108.addEventListener('click', func108);
+function func108(){
+    button108.innerHTML = 'Full Stack App'
+    button108.style.color = 'green';
+    button108.style.fontSize = '1.2em;';
+    div1.innerHTML = "FocusOn: React, Express, Node.js, JSX, redux, react-redux, hook, frontend, backend, database, building full-stack App)"
+    div1.style.color= "green" 
+};
+
+const button000 = document.getElementById('button000');
+button000.addEventListener('click', func00);
+function func00(){
+    button000.innerHTML = '10.2022-02.2023'
+    button000.style.color = 'blue';
+    button000.style.fontSize = '1.2em;';
+};
+const button001 = document.getElementById('button001');
+button001.addEventListener('click', func001);
+function func001(){
+    button001.innerHTML = 'web/App development'
+    button001.style.color = 'red';
+    button001.style.fontSize = '1.2em;';
+    }
+const button002 = document.getElementById('button002');
+button002.addEventListener('click', func002);
+function func002(){
+    button002.innerHTML = 'ActivateWork (Per Scholas)Software Training Boot Camp Certification, Denver CO'
+    button002.style.color = 'green';
+    button002.style.fontSize = '1.2em;';
+    div1.innerHTML = "Javascript, HTML, CSS, Github, Postman, MongoDB, Mongoose, Trello, React, Node.js, Express"
+    div1.style.color= "green" 
+    }
+
+    const button003 = document.getElementById('button003');
+    button003.addEventListener('click', func003);
+    function func003(){
+        button003.innerHTML = '07.2022-09.2022'
+        button003.style.color = 'blue';
+        button003.style.fontSize = '1.2em;';
+    };
+    const button004 = document.getElementById('button004');
+    button004.addEventListener('click', func004);
+    function func004(){
+        button004.innerHTML = 'JAVA, Python'
+        button004.style.color = 'red';
+        button004.style.fontSize = '1.2em;';
+        }
+    const button005 = document.getElementById('button005');
+    button005.addEventListener('click', func005);
+    function func005(){
+        button005.innerHTML = 'Learning by myself'
+        button005.style.color = 'green';
+        button005.style.fontSize = '1.2em;';
+        div1.innerHTML = "Java, Python"
+        div1.style.color= "green"
+        }
+        const button006 = document.getElementById('button006');
+        button006.addEventListener('click', func006);
+        function func006(){
+            button006.innerHTML = '08.2020-08.2021'
+            button006.style.color = 'blue';
+            button006.style.fontSize = '1.2em;';
+        };
+        const button007 = document.getElementById('button007');
+        button007.addEventListener('click', func007);
+        function func007(){
+            button007.innerHTML = 'LLM Program, Law master degree'
+            button007.style.color = 'red';
+            button007.style.fontSize = '1.2em;';
+            }
+        const button008 = document.getElementById('button008');
+        button008.addEventListener('click', func008);
+        function func008(){
+            button008.innerHTML = 'University of Colorado Law School, Boulder, CO. (Law master’s degree)'
+            button008.style.color = 'green';
+            button008.style.fontSize = '1.2em;';
+            div1.innerHTML = "Contract Law, Intellectual Property Law(Patent, Copyright, Information Security), Tort Law"
+            div1.style.color= "green" 
+            }
+        const button009 = document.getElementById('button009');
+        button009.addEventListener('click', func009);
+        function func009(){
+            button009.innerHTML = '08.2018-05.2019'
+            button009.style.color = 'blue';
+            button009.style.fontSize = '1.2em;';
+        };
+        const button010 = document.getElementById('button010');
+        button010.addEventListener('click', func010);
+        function func010(){
+            button010.innerHTML = 'Paralegal'
+            button010.style.color = 'red';
+            button010.style.fontSize = '1.2em;';
+            }
+        const button011 = document.getElementById('button011');
+        button011.addEventListener('click', fun011);
+        function fun011(){
+            button011.innerHTML = 'Community College of Denver, Denver, CO'
+            button011.style.color = 'green';
+            button011.style.fontSize = '1.2em;';
+            div1.innerHTML = "Civil Law, Legal writing, Legal research, Constitutional Law"
+            div1.style.color= "green" 
+            }
+        
+            const button012 = document.getElementById('button012');
+            button012.addEventListener('click', func012);
+            function func012(){
+                button012.innerHTML = '09.2010-06.2013:'
+                button012.style.color = 'blue';
+                button012.style.fontSize = '1.2em;';
+            };
+            const button013 = document.getElementById('button013');
+            button013.addEventListener('click', func013);
+            function func013(){
+                button013.innerHTML = 'Law Master'
+                button013.style.color = 'red';
+                button013.style.fontSize = '1.2em;';
+                }
+            const button014 = document.getElementById('button014');
+            button014.addEventListener('click', fun014);
+            function fun014(){
+                button014.innerHTML = 'South China University of Technology, Guangdong, China (M.S. Law)'
+                button014.style.color = 'green';
+                button014.style.fontSize = '1.2em;';
+                div1.innerHTML = "Contract Law, IP Law, International Law, Civil Law..."
+                div1.style.color= "green" 
+                }
+            const button015 = document.getElementById('button015');
+            button015.addEventListener('click', func015);
+            function func015(){
+                button015.innerHTML = '09.1995-06.1999:'
+                button015.style.color = 'blue';
+                button015.style.fontSize = '1.2em;';
+            };
+            const button016 = document.getElementById('button016');
+            button016.addEventListener('click', func016);
+            function func016(){
+                button016.innerHTML = 'Hydropower structure engineering'
+                button016.style.color = 'red';
+                button016.style.fontSize = '1.2em;';
+                }
+            const button017 = document.getElementById('button017');
+            button017.addEventListener('click', fun017);
+            function fun017(){
+                button017.innerHTML = 'South China University of Technology, Guangdong, China'
+                button017.style.color = 'green';
+                button017.style.fontSize = '1.2em;';
+                div1.innerHTML = "Hydropower engineering"
+                div1.style.color= "green" 
+                }
+{/*.2013: South China University of Technology, Guangdong, China (M.S. Law) </li>
+<li>South China University of Technology, Guangdong, China (B.S. in Structural Engineering)</li>                                                                                                 
+  */}
+const button0 = document.getElementById('button0');
+button0.addEventListener('click', func0);
+function func0(){
+    button0.innerHTML = '2021 – Present'
+    button0.style.color = 'blue';
+    button0.style.fontSize = '1.2em;';
+};
+
+const button1 = document.getElementById('button1');
+button1.addEventListener('click', func1);
+function func1(){
+    button1.innerHTML = 'Legal Interpreter / Translator'
+    button1.style.color = 'red';
+    button1.style.fontSize = '1.2em;';
+    }
+
+const button2 = document.getElementById('button2');
+button2.addEventListener('click', func2);
+function func2(){
+    button2.innerHTML = 'Rocket Mountain Immigration Advocacy Network, Denver, CO.'
+    button2.style.color = 'green';
+    button2.style.fontSize = '1.2em;';
+    div1.innerHTML = "Translate English legal documents into Chinese;Interpret and explain the English legal conversation for the Mandarin/Cantonese client;Explain legal terms to Clients;Research and write contracts, agreements, and proposals. File motions, writs, and briefs for each case."
+    div1.style.color= "green" 
+    }
+
+    const button3 = document.getElementById('button3');
+    button3.addEventListener('click', func3);
+    function func3(){
+        button3.innerHTML = '06.2021-08.2021'
+        button3.style.color = 'blue';
+        button3.style.fontSize = '1.2em;';
+    };
+    
+    const button4 = document.getElementById('button4');
+    button4.addEventListener('click', func4);
+    function func4(){
+        button4.innerHTML = 'Legal Consultant'
+        button4.style.color = 'red';
+        button4.style.fontSize = '1.2em;';
+        }
+    
+    const button5 = document.getElementById('button5');
+    button5.addEventListener('click', func5);
+    function func5(){
+        button5.innerHTML = 'Gibson Arnold & Associates, Inc., Denver, CO.'
+        button5.style.color = 'green';
+        button5.style.fontSize = '1.2em;';
+        div1.innerHTML = "Worked on a sensitive information review project. Reviewed, classified, and collected sensitive information from a variety of various data breach documents. Identified, analyzed, and interpreted sensitive legal data to ensure the integrity and security of the data.."
+        div1.style.color= "green" 
+        }
+    const button6 = document.getElementById('button6');
+    button6.addEventListener('click', func6);
+    function func6(){
+        button6.innerHTML = '05.2014–12.2016'
+        button6.style.color = 'blue';
+        button6.style.fontSize = '1.2em;';
+    };
+    
+    const button7 = document.getElementById('button7');
+    button7.addEventListener('click', func7);
+    function func7(){
+        button7.innerHTML = 'Associate Lawyer'
+        button7.style.color = 'red';
+        button7.style.fontSize = '1.2em;';
+        }
+    
+    const button8 = document.getElementById('button8');
+    button8.addEventListener('click', func8);
+    function func8(){
+        button8.innerHTML = 'Guangdong VGE Law Firm, Guangdong, China'
+        button8.style.color = 'green';
+        button8.style.fontSize = '1.2em;';
+        div1.innerHTML = "Analyzed and applied legal principles and performed legal research to support litigation strategy. Prepared and reviewed contracts and other legal documents following applicable regulations. Interviewed and represented in court for clients."
+        div1.style.color= "green" 
+        }
+    
+    const button9 = document.getElementById('button9');
+    button9.addEventListener('click', func9);
+    function func9(){
+        button9.innerHTML = '09.1999–09.2010'
+        button9.style.color = 'blue';
+        button9.style.fontSize = '1.2em;';
+    };
+    
+    const button10 = document.getElementById('button10');
+    button10.addEventListener('click', func10);
+    function func10(){
+        button10.innerHTML = 'Civil Engineer'
+        button10.style.color = 'red';
+        button10.style.fontSize = '1.2em;';
+        }
+    
+    const button11 = document.getElementById('button11');
+    button11.addEventListener('click', func11);
+    function func11(){
+        button11.innerHTML = 'Guangdong No.3 Water Resources Hydropower Engineering Company, Guangdong, China'
+        button11.style.color = 'green';
+        button11.style.fontSize = '1.2em;';
+        div1.innerHTML = "Developed and implemented technical solutions to maintain and improve client infrastructure. Prepared monthly budgeting reports, managed and instructed construction site workers. Analyzed and assessed project costs for materials and project drawings. Conducted cost analysis according to the construction specifications ensuring sound pricing strategies and models."
+        div1.style.color= "green" 
+        }
